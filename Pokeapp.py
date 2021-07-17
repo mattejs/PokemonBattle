5ecdc088b35537192ce1095ffdacc4c25431ba45
Matej <msarcevic997@gmail.com>
2021-07-17 18:25:21 +0200

    Pokeapp changed

--- Pokeapp.py
+++ Pokeapp.py
@@ -46,7 +46,7 @@ def home():
     
 @app.route('/list', methods=['GET', 'POST'])
 def lista():    
-    items = PreviousBattles.query.all()
+    items = PreviousBattles.query.order_by(id.desc()).limit(6).all()
     return render_template('list.html', items=items)
 
 @app.route('/battle', methods=['POST','GET'])
